062bf27dc62130fc4a8ec5711bac448a86abdc4a
shegy28 <shegy28@gmail.com>
2023-02-22 17:56:13 +0100

    Used join query for multiple tables

--- schema.sql
+++ schema.sql
@@ -12,3 +12,39 @@ CREATE TABLE animals(
 
 ALTER TABLE animals
 ADD species varchar(255);
+
+-- Multiple tables
+
+CREATE TABLE owners (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    full_name varchar(255),
+    age int,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE species (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name varchar(255),
+    PRIMARY KEY (id)
+);
+
+ALTER TABLE animals
+DROP COLUMN species;
+
+ALTER TABLE animals 
+ADD COLUMN species_id INT;
+
+ALTER TABLE animals
+ADD CONSTRAINT fk_animals_spec
+FOREIGN KEY (species_id)
+REFERENCES species(id);
+
+ALTER TABLE animals 
+ADD COLUMN owner_id INT;
+
+ALTER TABLE animals
+ADD CONSTRAINT fk_animals_owner
+FOREIGN KEY (owner_id)
+REFERENCES owners(id);
+
+
